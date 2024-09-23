378ba4cb00425192407e940b1aede2e71be3f1d9
zqhgithubuser <129922525+zqhgithubuser@users.noreply.github.com>
2024-09-23 15:35:00 +0000

    发送邮件

--- send_email.py
+++ send_email.py
@@ -3,14 +3,40 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from dataclasses import dataclass
 
-# 配置信息
-port = 465
-smtp_server = "smtp.163.com"
-login = "15815085647@163.com"
-password = "XXXX"
 
-sender_email = login
+@dataclass
+class Config:
+    smtp_server: str
+    port: int
+    login_email: str
+    password: str
+
+
+def send_email(config: Config, receiver_email: str, subject: str,
+               html_content: str):
+    sender_email = config.login_email
+
+    message = MIMEMultipart()
+    message["Subject"] = subject
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message.attach(MIMEText(html_content, "html"))
+
+    with smtplib.SMTP_SSL(config.smtp_server, config.port) as server:
+        server.login(config.login_email, config.password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+        print("邮件发送成功!")
+
+
+config = Config(
+    smtp_server="smtp.163.com",
+    port=465,
+    login_email="15815085647@163.com",
+    password="XXXX"
+)
+
 receiver_email = "3535521945@qq.com"
 
 html = """\
@@ -24,13 +50,5 @@ html = """\
 </html>
 """
 
-message = MIMEMultipart()
-message["Subject"] = "这是一封HTML邮件"
-message["From"] = sender_email
-message["To"] = receiver_email
-message.attach(MIMEText(html, "html"))
-
-with smtplib.SMTP_SSL(smtp_server, port) as server:
-  server.login(login, password)
-  server.sendmail(sender_email, receiver_email, message.as_string())
-  print("邮件发送成功!")
+if __name__ == "__main__":
+    send_email(config, receiver_email, "这是一封HTML邮件", html)
