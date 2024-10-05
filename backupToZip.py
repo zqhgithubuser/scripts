af6f6ba4f63ac17bfc1a6487e4ef805231560778
zqhgithubuser <129922525+zqhgithubuser@users.noreply.github.com>
2024-10-05 06:36:03 +0000

    备份目录

--- backupToZip.py
+++ backupToZip.py
@@ -5,20 +5,21 @@ import shutil
 
 
 def backup(dir_path, target_dir):
-    fullDirname = os.path.abspath(dir_path)
-    dirname = os.path.dirname(fullDirname)
-    basename = os.path.basename(fullDirname)
+    fullpath = os.path.abspath(dir_path)
+    dirname = os.path.dirname(fullpath)
+    basename = os.path.basename(fullpath)
 
     tz = pytz.timezone("Asia/Shanghai")
     today = datetime.now(tz).date()
-    zipFilename = os.path.join(target_dir, f"{basename}_{today}")
+    zipBasename = os.path.join(target_dir, f"{basename}_{today}")
+    zipFilename = zipBasename + ".zip"
     if os.path.exists(zipFilename):
-        print("Zip file already exists.")
+        print(f"Zip file {zipFilename} already exists.")
         return
 
     print(f"Creating {zipFilename}...")
     shutil.make_archive(
-        zipFilename,
+        zipBasename,
         "zip",
         root_dir=dirname,
         base_dir=basename
