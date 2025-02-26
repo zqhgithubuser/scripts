e12a93e4b8f195c3640b157c3a9b133f92221e60
zqhgithubuser <129922525+zqhgithubuser@users.noreply.github.com>
2025-02-26 23:31:57 +0800

    阿里源

--- install_docker.sh
+++ install_docker.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
-DOWNLOAD_URL=https://mirrors.aliyun.com/docker-ce
+repository_url=https://mirrors.aliyun.com/docker-ce
 
-get_distribution() {
-    local lsb_dist="$(. /etc/os-release && echo "$ID")"
-    echo "$lsb_dist"
+get_os_type() {
+    local os_type="$(. /etc/os-release && echo "$ID")"
+    echo "$os_type"
 }
 
-get_dist_version() {
-    local dist_version=$(. /etc/os-release && echo "$VERSION_CODENAME")
-    echo "$dist_version"
+get_os_version() {
+    local os_version=$(. /etc/os-release && echo "$VERSION_CODENAME")
+    echo "$os_version"
 }
 
 do_install() {
-    local lsb_dist=$( get_distribution )    
+    local os_type=$( get_os_type )    
     echo "Installing Docker..."
-    case "$lsb_dist" in
+
+    case "$os_type" in
         ubuntu)
             apt update && apt install -y ca-certificates curl
             install -m 0755 -d /etc/apt/keyrings
             curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
             chmod a+r /etc/apt/keyrings/docker.asc
             echo \
-                "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] $DOWNLOAD_URL/linux/ubuntu \
-                $( get_dist_version ) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+                "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] $repository_url/linux/ubuntu \
+                $( get_os_version ) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
             apt update && apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
             ;;
         centos|rocky)
             yum install -y yum-utils
-            yum-config-manager --add-repo "$DOWNLOAD_URL"/linux/centos/docker-ce.repo
+            yum-config-manager --add-repo "$repository_url"/linux/centos/docker-ce.repo
             yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
             systemctl enable docker
             systemctl start docker
@@ -40,4 +41,5 @@ do_install() {
 }
 
 do_install
+
 echo "Completed!"
