923ad4ff5cb75042efa789007ba17568b9303f33
zqhgithubuser <129922525+zqhgithubuser@users.noreply.github.com>
2025-02-26 23:26:05 +0800

    Harbor

--- install_harbor.sh
+++ install_harbor.sh
@@ -1,23 +1,28 @@
 #/bin/bash
 
-HARBOR_VERSION=2.10.1
-HARBOR_NAME="harbor.zqh.org"
-HARBOR_ADMIN_PASSWORD="123456"
+harbor_version=2.10.1
+harbor_host="harbor.zqh.org"
+harbor_admin_password="123456"
 
 install_harbor() {
     echo "Installing Harbor..."
-    if [ ! -e  harbor-offline-installer-v"$HARBOR_VERSION".tgz ]; then
-        wget https://github.com/goharbor/harbor/releases/download/v"$HARBOR_VERSION"/harbor-offline-installer-v"$HARBOR_VERSION".tgz || echo "Failed to download harbor installer."
+
+    local installer="harbor-offline-installer-v$harbor_version.tgz"
+    if [ ! -e  harbor-offline-installer-v"$harbor_version".tgz ]; then
+        if ! wget https://github.com/goharbor/harbor/releases/download/v"$harbor_version"/"$installer"; then
+            echo "Failed to download harbor installer."
+            exit 1
     fi
-    tar xvf harbor-offline-installer-v"$HARBOR_VERSION".tgz -C /usr/local/
+    tar xvf "$installer" -C /usr/local/
     cd /usr/local/harbor
     cp harbor.yml.tmpl harbor.yml
-    sed -ri "/^hostname/s/reg.mydomain.com/${HARBOR_NAME}/" harbor.yml
+
+    sed -ri "/^hostname/s/reg.mydomain.com/$harbor_host/" harbor.yml
     sed -ri "/^https/s/(https:)/#\1/" harbor.yml
     sed -ri "s/(port: 443)/#\1/" harbor.yml
     sed -ri "/certificate:/s/(.*)/#\1/" harbor.yml
     sed -ri "/private_key:/s/(.*)/#\1/" harbor.yml
-    sed -ri "s/Harbor12345/${HARBOR_ADMIN_PASSWORD}/" harbor.yml
+    sed -ri "s/Harbor12345/$harbor_admin_password/" harbor.yml
     sed -i 's#^data_volume: /data#data_volume: /data/harbor#' harbor.yml
     
     if /usr/local/harbor/install.sh; then
@@ -29,7 +34,7 @@ install_harbor() {
         
     echo "Completed!"
     echo "-------------------------------------------------------------------"
-    echo "URL: http://${HARBOR_NAME}" 
+    echo "URL: http://$harbor_host" 
 }
 
 install_harbor
